a8433addabb8738ac717eba43c82127c4b9faa7c
Deelolade <Oluwanisholahabeeb@icloud.com>
2024-12-08 23:24:49 +0100

    testing progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
+      <Header/>
       <h1>Hello, Metro!</h1>
       <p>Welcome to the Metropolitan Museum of Art!</p>
       <p>This is a demo application.</p>
       <p>Feel free to explore the different components and see how they interact.</p>
+      <Footer/>
     </div>
   )
 }

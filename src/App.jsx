06d94fea5fb021ebfaf7df0cdc75d495fc4f082a
Deelolade <Oluwanisholahabeeb@icloud.com>
2024-12-08 22:44:17 +0100

    Trying Githubconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Hello, Metro!</h1>
+      <p>Welcome to the Metropolitan Museum of Art!</p>
+      <p>This is a demo application.</p>
+      <p>Feel free to explore the different components and see how they interact.</p>
+      <p>Enjoy your visit!</p>
+    </div>
   )
 }
 

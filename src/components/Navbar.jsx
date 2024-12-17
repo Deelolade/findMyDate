7bd0969d33e49a8ce9bd35b52d8432516335b8f1
Deelolade <Oluwanisholahabeeb@icloud.com>
2024-12-17 03:48:49 +0100

    created section branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className='flex w-screen h-[10vh] md:h-[8vh] bg-white shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_1px_4px_-1px_rgba(0,0,0,0.06)]'>
-    <div className="logo flex items-center mx-7 ">
+    <div className="logo flex items-center  mx-7 ">
       <h1 className='font-bold text-justify' >
       FindMyDate
       </h1>
